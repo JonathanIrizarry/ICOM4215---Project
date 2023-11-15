0e9ca9f9f5e47c8b1df874a1421bb1415e096bc4
JonathanIrizarry <jonathan.irizarry3@upr.edu>
2023-11-14 20:15:40 -0400

    Create rom512x8.v
    Added Instruction Memory module from Phase 1

---
+++ rom512x8.v
@@ -0,0 +1,6 @@
+module rom512x8 (output reg [31:0] DataOut, input [8:0] Address);
+reg [7:0] Mem[0:511]; //512 localizaciones de 8 bits
+always @ (Address)
+		DataOut <= {Mem[Address], Mem[Address + 1], Mem[Address + 2], Mem[Address + 3]};
+endmodule
+
