c1a6eb20ae71cdbb8156e783e91f09ba83fa73be
JonathanIrizarry <jonathan.irizarry3@upr.edu>
2023-11-17 21:17:43 -0400

    Update module SimplePipeline.v
    Added register initializations for each pipeline phase

--- module SimplePipeline.v
+++ module SimplePipeline.v
@@ -36,28 +36,13 @@ module SimplePipeline(
     wire [15:0] immediate;
     wire [5:0] funct;
 
-    // Bus for control signals
-    wire [12:0] control_bus;
-
-
 
     // Initialization of Instruction Memory
     instr_mem imem (
         .DataOut(instruction_reg),
         .Address(address)
     );
-
-//Preload Instruction Memory
-initial begin
-	fi = $fopen("input.txt","r");
-	address = 9'b000000000;
-	while (!$feof(fi)) begin
-		code = $fscanf(fi, "%b", data);
-		imem.Mem[address] = data;
-		address = address + 1;
-	end
-	$fclose(fi);
-end
+	
 
       // Etapa IF (Fetch)
     always @(posedge clk or posedge reset) begin
@@ -80,6 +65,7 @@ end
         if (reset) begin
             // Initialize control signals
             // Add your initialization logic here if needed
+			ta_instr_reg = 1'b0;
         end else begin
             // Pass the instruction through the control unit
             PPU_Control_Unit control_unit(
@@ -96,7 +82,8 @@ end
                 .ID_Enable_HI(ID_Enable_HI),
                 .ID_Enable_LO(ID_Enable_LO)
             );
-       
+			
+			ta_instr_reg = control_output[7];
           
         end
     end
@@ -104,9 +91,17 @@ end
     // Etapa EX (Ejecución)
     always @(posedge clk or posedge reset) begin
         if (reset) begin
-            // Inicializar registros en caso de reset
+            // Inicializar registros en caso de reset'
+			alu_op_reg = 3'b000;
+			branch_reg = 1'b0;
+			load_instr_reg = 1'b0;
+			rf_enable_reg = 1'b0;
         end else begin
             // Lógica de la etapa EX, como operaciones aritméticas y lógicas
+			alu_op_reg = control_output[3:1];
+			branch_reg = control_output[6];
+			load_instr_reg = control_output[4];
+			rf_enable_reg = control_output[5];
             if (le_alu) begin
                 // Perform ALU operation
             end
@@ -117,8 +112,21 @@ end
     always @(posedge clk or posedge reset) begin
         if (reset) begin
             // Inicializar registros en caso de reset
+			mem_size_reg = 2'b00;
+			mem_se_reg = 1'b0;
+			mem_rw_reg = 1'b0; 
+			mem_enable_reg = 1'b0;
+			load_instr_reg = 1'b0;		
+			rf_enable_reg = 1'b0;			
         end else begin
             // Lógica de la etapa MEM, como acceso a memoria (load o store)
+			mem_size_reg = control_output[9:8];
+			mem_se_reg = control_output[11];
+			mem_rw_reg = control_output[10];
+			mem_enable_reg = control_output[14];
+			load_instr_reg = control_output[4];
+			rf_enable_reg = control_output[5];
+			//mem_mux_enable = ??
             if (le_mem) begin
                 // Perform memory operation
             end
@@ -129,8 +137,15 @@ end
     always @(posedge clk or posedge reset) begin
         if (reset) begin
             // Inicializar registros en caso de reset
+			rf_enable_reg = 1'b0;
+			hi_enable_reg = 1'b0;
+			lo_enable_reg = 1'b0;
         end else begin
-            result_reg <= mem_result; // En una implementación real, puedes seleccionar entre alu_result y mem_result según la instrucción
+            result_reg <= mem_result; 
+			rf_enable_reg = control_output[5];
+			hi_enable_reg = control_output[12];
+			lo_enable_reg = control_output[13];
+			// En una implementación real, puedes seleccionar entre alu_result y mem_result según la instrucción
         end
     end
 
@@ -168,7 +183,7 @@ module PPU_Control_Unit (
     parameter JR_Funct = 6'b001000;
     parameter LUI_OP = 6'b001111;
 
-
+	
     // Control signals
     assign ID_Shift_Imm  = (instruction[31:26] == ADDIU_Op) ? 1'b1 : 1'b0;
     assign ID_ALU_OP     = (instruction[31:26] == ADDIU_Op) ? 3'b001
@@ -184,11 +199,11 @@ module PPU_Control_Unit (
     assign ID_MEM_SE     = (instruction[31:26] == LBU_Op) ? 1'b1 : 1'b0; // Assuming sign-extension for LBU_Op
     assign ID_Enable_HI  = (instruction[31:26] == R_TYPE) ? 1'b1 : 1'b0;
     assign ID_Enable_LO  = (instruction[31:26] == R_TYPE) ? 1'b1 : 1'b0;
-
+	assign ID_MEM_Enable  = (instruction[31:26] == SB_OP) ? 1'b1 : 1'b0;
 
      // Concatenate control signals based on the mux input
     always @* begin
-        control_output = (S) ? {ID_Shift_Imm, ID_ALU_OP, ID_Load_Instr, ID_RF_Enable, ID_B_Instr, ID_TA_Instr, ID_MEM_Size, ID_MEM_RW, ID_MEM_SE, ID_Enable_HI, ID_Enable_LO} : 12'b0;
+        control_output = (S) ? {ID_Shift_Imm, ID_ALU_OP, ID_Load_Instr, ID_RF_Enable, ID_B_Instr, ID_TA_Instr, ID_MEM_Size, ID_MEM_RW, ID_MEM_SE, ID_Enable_HI, ID_Enable_LO, ID_MEM_Enable} : 15'b0;
     end
 
 endmodule
