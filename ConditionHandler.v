8d70fa6d1bed3f2a9c1352e7b761a9704dd80ca0
edgarsuarez123 <edgar.suarez1@upr.edu>
2023-12-03 21:22:09 -0400

    added condition handler module

--- ConditionHandler.v
+++ ConditionHandler.v
@@ -9,10 +9,78 @@ module Condition_Handler(
 
 always @* begin
 
+    handler_Out <= B_instr;
+end
 
+endmodule
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+module LogicBox(
+    input Handler_B_instr;
+    input unconditional_jump_signal;
+    output reg logicbox_out;
+);
+
+    always @* begin
+        if(B_instr == 1'b1 || unconditional_jump_signal == 1'b1)begin
+                logicbox_out <= 1'b1;
+        end else begin
+                logicbox_out <= 1'b0;
+        end
+
+
+    end
+endmodule
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+module LogicBox_mux(
+    input logicbox_out; // 1 bit
+    input [31:0] IF_mux;  
+    input [31:0] nPC_input;  
+    output reg [31:0] Logic_mux_output;  
+);
+    always @* begin
+        if(logicbox_out == 1'b1) begin
+            Logic_mux_output <= IF_mux; //TA or rs
+
+
+        end else begin
+             Logic_mux_output <= nPC_input; // npc
+
+        end
+       
+
+    end 
+
+endmodule
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+module IF_Mux(
+    input [31:0] EX_TA;
+    input [31:0] ID_TA;   //concatenau 30 bits y multiplicau 32?? 
+    input [5:0] rs; //chequear que manda
+    input TA_instruction; 
+    output reg [31:0]mux_out;  
+);
+
+always @* begin  
+    if(TA_instruction == 1'b1)begin
+        mux_out <= ID_TA;   // cheuqear differencia entre id y EX TA
+    end else begin
+        mux_out = {26'b0 , rs};
+    end
 end
 
 
-
-
 endmodule
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
--- IFID_Stage.v
+++ IFID_Stage.v
@@ -3,10 +3,9 @@ module IFID_Stage (
     input reset,
     input wire [31:0] instruction_in,
     output reg [31:0] instruction_out,
-	//input [16:0] control_signals,
-	//output reg ta_instr_reg
+	input [8:0] input_pc;
     output reg [25:0] address_26, // bit 25:0 de instruction 
-    output reg [8:0] PC, //bit8:0
+    output reg [8:0] PC, //bit8:0  entrada desde PC
     output reg [25:21] rs, //bit 25:21
     output reg [20:16] rt, //bit 20:16
     output reg [15:0] imm16, //bit 15:0
@@ -18,15 +17,38 @@ module IFID_Stage (
     always @(posedge clk or posedge reset) begin
 		if (reset) begin
 				instruction_out <= 32'b0;
+                address_26 <= 0;
+                PC <= 0;
+                rs <= 0;
+                rt <= 0;
+                imm16 <= 0;
+                opcode <= 0;
+                rd <= 0;
+
 			end else begin
-				instruction_out <= instruction_in;
-                address_26 <= instruction_in[25:0];
-                PC <= instruction_in[8:0];
-                rs <= instruction_in[25:21];
-                rt <= instruction_in[20:16];
-                imm16 <= instruction_in[15:0];
-                opcode <= instruction_in[31:26];
-                rd <= instruction_in[15:11];
+				instruction_out <= instruction_in; 
+                // PC <= instruction_in[8:0]; entrada desde PC 
+                if(instruction_in[31:26] == 6'b0) begin
+                    opcode <= instruction_in[31:26];
+                    rd <= instruction_in[15:11];
+                    rs <= instruction_in[25:21];
+                    rt <= instruction_in[20:16];
+                    
+                end else if(instruction_in[31:26] == 6'b000011) begin
+                    address_26 <= instruction_in[25:0];
+                    opcode <= instruction_in[31:26];
+
+                end else begin
+                    opcode <= instruction_in[31:26];
+                    rs <= instruction_in[25:21];
+                    rt <= instruction_in[20:16];
+                    imm16 <= instruction_in[15:0];
+    
+                end
+
+
+
+
     end
 	end
 endmodule
