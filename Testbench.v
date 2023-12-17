4ba221b12e0c81c4d049424a443a8575ce544e56
edgarsuarez123 <edgar.suarez1@upr.edu>
2023-12-17 13:21:53 -0400

    fixes

--- Testbench.v
+++ Testbench.v
@@ -448,7 +448,7 @@ IF_Mux if_mux_inst(
 
 LogicBox logicBox_inst(
   .Handler_B_instr(Condition_handler_out),
-  .unconditional_jump_signal(ex_wire[19]/*mux_out_wire[19]*/ ), // chekc
+  .unconditional_jump_signal(/*ex_wire[19]*/mux_out_wire[19] ), // chekc
   .logicbox_out(logicBox_mux_out)
 );
 
@@ -723,27 +723,27 @@ MEMWB_Stage wb_instance(
 
 
 
-$monitor("\n\n\nPC: %d\n---------------------------------\
-		\nAlu_A: %d | Alu_B: %d\
-		\nAlu_Op: %b | Alu_Out: %d\
-		\nID_RF: %b\
-		\nEX_RF: %b\
-		\nMEM_RF: %b\
-		\nWB_RF: %b\
-		\n PC8: %d\
-		\n DataMEM_out: %d\
-		\n MEM_ALUOut: %d\
-		\n Valor_GuardarEnRF: %d\
-        \n--------------------------------------------------\
-		\n Target Address\
-		\n  ConcatenatedPC:%d | PC4*imm16: %d | ID_TA: %d | EX_TA: %d | rs_PC: %d \
-		\n IF_MUxOut: %d |  Npc: %d | PC_IN: %d | Instruction: %b \
-		\n  ConditionHandlerout: %b |  UNcodnitionalSignalID: %b | LogicBoxOut: %b | TaINstrID: %b\                                                ",
-        pc_wire_out,  PA_out_Ex, N_ALU, alu_op_reg, alu_out, 
-	    mux_out_wire[9], ID_rf_enable_reg, mem_rf_enable_reg, MEM_rf_enable_reg,mem_pc8_out, dataMem_Out,mem_alu_out, mux_WB_out,
-		concatenated_result_out, addedPCFourAndFourTimesimmSixteen, targetAddress_in, targetAddress_out, mux_PA_out,
-		if_mux_out, npc_wire_out, pc_wire_in, instruction_wire_out, Condition_handler_out, mux_out_wire[21], logicBox_mux_out,mux_out_wire[7]
-		);
+// $monitor("\n\n\nPC: %d\n---------------------------------\
+// 		\nAlu_A: %d | Alu_B: %d\
+// 		\nAlu_Op: %b | Alu_Out: %d\
+// 		\nID_RF: %b\
+// 		\nEX_RF: %b\
+// 		\nMEM_RF: %b\
+// 		\nWB_RF: %b\
+// 		\n PC8: %d\
+// 		\n DataMEM_out: %d\
+// 		\n MEM_ALUOut: %d\
+// 		\n Valor_GuardarEnRF: %d\
+//         \n--------------------------------------------------\
+// 		\n Target Address\
+// 		\n  ConcatenatedPC:%d | PC4*imm16: %d | ID_TA: %d | EX_TA: %d | rs_PC: %d \
+// 		\n IF_MUxOut: %d |  Npc: %d | PC_IN: %d | Instruction: %b \
+// 		\n  ConditionHandlerout: %b |  UNcodnitionalSignalID: %b | LogicBoxOut: %b | TaINstrID: %b\                                                ",
+//         pc_wire_out,  PA_out_Ex, N_ALU, alu_op_reg, alu_out, 
+// 	    mux_out_wire[9], ID_rf_enable_reg, mem_rf_enable_reg, MEM_rf_enable_reg,mem_pc8_out, dataMem_Out,mem_alu_out, mux_WB_out,
+// 		concatenated_result_out, addedPCFourAndFourTimesimmSixteen, targetAddress_in, targetAddress_out, mux_PA_out,
+// 		if_mux_out, npc_wire_out, pc_wire_in, instruction_wire_out, Condition_handler_out, mux_out_wire[21], logicBox_mux_out,mux_out_wire[7]
+//  		);
 
 
 // $monitor("\n\n\nPC: %d\n---------------------------------\
@@ -767,18 +767,18 @@ $monitor("\n\n\nPC: %d\n---------------------------------\
 // 		);
 
 
-// $monitor("\n\n\nPC: %d\n---------------------------------\
-        // \nAddress: %b\n--------------------------------------\
-        // \nR5: %d | R6: %d\
-        // \nR16: %d | R17: %d\
-		// \nR18: %d\
-        // \n--------------------------------------------------",
-        // pc_wire_out,
-        // mem_alu_out,
-        // Q5, Q6,
-        // Q16, Q17, Q18);
+$monitor("\n\n\nPC: %d\n---------------------------------\
+        \nAddress: %b\n--------------------------------------\
+        \nR5: %d | R6: %d\
+        \nR16: %d | R17: %d\
+		\nR18: %d\
+        \n--------------------------------------------------",
+        pc_wire_out,
+        mem_alu_out,
+        Q5, Q6,
+        Q16, Q17, Q18);
 
-	  end
+ 	  end
 
 
  // Printing Data from each phase
